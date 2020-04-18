52a59fa141d987206ae92ff1c825c028a6d60baa
Lucas Schneeberger <lucas.schneeberger@epfl.ch>
2020-04-18 18:40:25 +0200

    findTime finds the time vector and the sampling time for an AirfoilMotion instance

---
+++ findTime.m
@@ -0,0 +1,27 @@
+function [t, Ts] = findTime(motion,c)
+N = length(motion.alpha);
+if isempty(motion.Ts)
+    switch class(motion)
+        case 'PitchingMotion'
+            % k = omega*c/(2*V)
+            if ~isempty(motion.freq)
+                freq = motion.freq;
+            else
+                freq = motion.k*2*motion.V/(2*pi*c);
+             end
+            if ~isempty(motion.f_pts)
+                Ts = motion.f_pts/freq; % rev/sampling interval * time/rev = time/sampling intervail
+            else
+                error('f_pts must be set on this data beforehand, e.g. using findSinus.')
+            end
+        case 'RampUpMotion'
+            % r = alphadot*c/(2*V)
+            if isempty(motion.Ts)
+                error('Computing the time vector without the sampling frequency Ts is impossible for a RampUpMotion object.')
+            end
+    end
+else
+    Ts = motion.Ts;
+end
+    t = 0:Ts:Ts*(N-1);
+end
