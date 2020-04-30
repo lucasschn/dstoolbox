eacdb5b6256b5f3a904d2b8c9cfecfcc2e7e349a
Lucas Schneeberger <lucas.schneeberger@epfl.ch>
2020-04-30 10:58:13 +0200

    Adapted Sheng method to a more modular structure

--- Airfoil.m
+++ Airfoil.m
@@ -3,9 +3,10 @@ classdef Airfoil < handle
         name
         c
         alpha_ds0
-        D1
+        pr
+        pl
         Talpha
-        r0 = 0.01 % reduced pitch rate for bilinear transition  
+        r0 = 0.03 % reduced pitch rate for bilinear transition  
         steady % corresponding steady curve
     end
     methods
@@ -25,27 +26,73 @@ classdef Airfoil < handle
         function b = b(obj)
             b = obj.c/2;
         end
-        function figs = Sheng(obj,varargin)
-            % argument is a series of RampUpMotions
+        function computeTalpha(obj,r,alpha_ds)
+            % computes Talpha based on a vector of reduced pitch rates r and
+            % corresponding dynamic stall angles alpha_ds. Talpha is the slope
+            % of the curve fitting alpha_ds as a function of r.
+            obj.pr = polyfit(r(r>=obj.r0),alpha_ds(r>=obj.r0),1);
+            D1 = obj.pr(1); % deg
+            obj.alpha_ds0 = obj.pr(2);
+            obj.Talpha = pi/180*D1; % rad
+            obj.pl = polyfit(r(r<obj.r0),alpha_ds(r<obj.r0),1);
+        end
+        function fig = Sheng(obj,varargin)
+            %% extract r and alpha_ds from arguments
+            % argument is a set of RampUpMotions
             r = -ones(size(varargin));
             alpha_ds = -ones(size(varargin));
-            for k=1:nargin-1
+            for k=1:nargin-1 % first argument is self
                 ramp = varargin{k};
                 if isempty(ramp.r)
-                    % with alphdot
+                    % compute it with alphadot
                     ramp.setPitchRate(obj);
                 end
-                %r(k-1) = ramp.rt(ramp.i_CConset);
-                if ramp.r>=obj.r0
-                    r(k) = ramp.r;
-                    alpha_ds(k) = ramp.alpha_CConset;
+                r(k) = ramp.r;
+                % Define experimental stall if necessary
+                if isempty(ramp.i_CConset)
+                    ramp.findExpOnset();
+                end
+                alpha_ds(k) = ramp.alpha_CConset;         
+            end
+            
+            % compute alpha_lag using Talpha and finds alpha_lagonset
+            obj.computeTalpha(r,alpha_ds)
+            alpha_lag_ds = -ones(size(varargin));
+            alpha_crit = -ones(size(varargin));
+            for k=1:nargin-1
+                ramp = varargin{k};
+                ramp.findModelOnset(obj); % alpha_lagonset = alpha_lag_ds only if Talpha is correct
+                % looking for the value of alpha_lag(alpha) at the point alpha_ds
+                if isempty(ramp.alpha)
+                    alpha_lag_ds(k) = interp1(ramp.analpha,ramp.analpha_lag,alpha_ds(k));
+                elseif isempty(ramp.i_continuous_grow)
+                    alpha_lag_ds(k) = interp1(ramp.alpha,ramp.alpha_lag,alpha_ds(k));
+                else % if alpha_continuous_grow is defined
+                    alpha_lag_ds(k) = interp1(ramp.alpha_continuous_grow,ramp.alpha_lag(ramp.i_continuous_grow),alpha_ds(k));
+                end
+                if ramp.r >= obj.r0
+                    alpha_crit(k) = obj.alpha_ds0;
+                else
+                    alpha_crit(k) = obj.steady.alpha_static_stall + (obj.alpha_ds0 - obj.steady.alpha_static_stall)*ramp.r/obj.r0;
                 end
             end
-            p = polyfit(r,alpha_ds,1);
-            obj.D1 = p(1);
-            obj.alpha_ds0 = p(2);
-            obj.Talpha = pi/180*obj.D1;
-            figs = figure;
+            
+            fig = obj.plotSheng(r,alpha_ds,alpha_lag_ds,alpha_crit);
+            
+        end
+        function fig = plotSheng(obj,r,alpha_ds,alpha_lag_ds,alpha_crit)
+            fig = obj.plotDS(r,alpha_ds);
+            figure(fig)
+            plot(r,[polyval(obj.pl,r(r<obj.r0)) polyval(obj.pr,r(r>=obj.r0))],'Color','r','DisplayName','Linear fitting','LineWidth',2)
+            title(sprintf('%s ($T_{\\alpha} = %.2f$)',obj.name,obj.Talpha),'interpreter','latex','FontSize',20)
+            plot(r,alpha_lag_ds,'.','DisplayName','\alpha_{ds} (lagged)','MarkerSize',20)
+            plot(r,alpha_crit,'--','DisplayName','\alpha_{ds,0}','LineWidth',2);
+            legend('Location','NorthWest','FontSize',20)
+        end
+    end
+    methods (Static)
+       function fig = plotDS(r,alpha_ds)
+            fig = figure;
             plot(r,alpha_ds,'.','DisplayName','\alpha_{ds} (exp)','MarkerSize',20)
             hold on
             grid on
@@ -53,33 +100,6 @@ classdef Airfoil < handle
             ylabel('\alpha_{ds} (°)','FontSize',20);
             ax = gca;
             ax.FontSize = 20;
-            title(sprintf('%s ($T_{\\alpha} = %.2f$)',obj.name,obj.Talpha),'interpreter','latex','FontSize',20)
-            plot(r,polyval(p,r),'DisplayName','Linear fitting','LineWidth',2)
-            alpha_lag_ds = zeros(size(alpha_ds));
-            for k=1:length(alpha_ds)
-                % retrieve which ramp corresponds to the current alpha_ds
-                for kk=1:nargin-1
-                    tmp = varargin{kk};
-                    if tmp.r == r(k)
-                        ramp = varargin{kk};
-                    end
-                end
-                % compute alpha_lag and finds alpha_lagonset
-                ramp.findModelOnset(obj);
-                % looking for the value of alpha_lag(alpha) at the point alpha_ds
-                if ramp.r >= obj.r0
-                    if isempty(ramp.alpha)
-                        alpha_lag_ds(k) = interp1(ramp.analpha,ramp.analpha_lag,alpha_ds(k));
-                    elseif isempty(ramp.i_continuous_grow)
-                        alpha_lag_ds(k) = interp1(ramp.alpha,ramp.alpha_lag,alpha_ds(k));
-                    else % if alpha_continuous_grow is defined
-                        alpha_lag_ds(k) = interp1(ramp.alpha_continuous_grow,ramp.alpha_lag(ramp.i_continuous_grow),alpha_ds(k));
-                    end
-                end
-            end
-            plot(r,alpha_lag_ds,'.','DisplayName','\alpha_{ds} (lagged)','MarkerSize',20)
-            plot(r,ones(size(r)).*obj.alpha_ds0,'--','DisplayName','\alpha_{ds,0}','LineWidth',2);
-            legend('Location','NorthWest','FontSize',20)
-        end
+       end
     end
 end
