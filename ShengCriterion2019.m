dcf4318c7db1395902ccedc0eb8fadedc5b57bc0
Lucas Schneeberger <lucas.schneeberger@epfl.ch>
2020-05-13 22:10:08 +0200

    Applied 3-step filtering to loads from SH2019.

--- ShengCriterion2019.m
+++ ShengCriterion2019.m
@@ -9,9 +9,7 @@ run('/Users/lucas/src/codes_smarth/labbook.m')
 c = [22,26,84,30];
 
 for k=1:length(c)
-    data = load(loadmat(LB(c(k)).ms,LB(c(k)).mpt),'raw','inert','avg','zero');
-    avg = data.avg;
-    zero = data.zero;
+    data = load(loadmat(LB(c(k)).ms,LB(c(k)).mpt),'raw','inert');
     raw = data.raw;
     inert = data.inert;
     inert.alpha = raw.alpha(raw.t>=0);
@@ -20,8 +18,19 @@ for k=1:length(c)
     evalin('base',sprintf('%s.setName()',msname))
     ramp = evalin('base',msname);
     ramp.setAlphaDot(LB(c(k)).alphadot) % in degrees
-    ramp.setCL(inert.Cl);
-    ramp.setCD(inert.Cd);
+    fc = 35;
+    fs = 1/ramp.Ts;
+    [b,a] = butter(5,fc/(fs/2));
+    Cl_filtered = filter(b,a,inert.Cl);
+    Cd_filtered = filter(b,a,inert.Cd);
+    Cl_ff = movmean(Cl_filtered,30);
+    Cd_ff = movmean(Cd_filtered,30);
+    fp = 1/3;
+    [b,a] = cheby2(6,20,36*fp/(fs/2));
+    Cl_fff = filter(b,a,Cl_ff);
+    Cd_fff = filter(b,a,Cd_ff);
+    ramp.setCL(Cl_fff);
+    ramp.setCD(Cd_fff);
     ramp.computeAirfoilFrame();
     ramp.isolateRamp();
     % Define stall
@@ -34,7 +43,7 @@ airfoil = Airfoil('flatplate',0.15);
 static = load('static_flatplate');
 airfoil.steady = SteadyCurve(static.alpha,static.Cn,14);
 % Define alpha_ds0 & compute Talpha
-figs = airfoil.Sheng(ms013,ms014,ms034,ms015);
+airfoil.Sheng(ms013,ms014,ms034,ms015);
 saveas(gcf,'fig/Sheng/ShengSH2019_dsr.png')
 %% Add Sheng's predicted stall angles to the figures
 for k=1:length(c)
@@ -44,4 +53,4 @@ for k=1:length(c)
     evalin('base',sprintf('plot(%s.alpha_lagonset*ones(2,1),fig%d.CurrentAxes.YLim,''b--'')',msname,k));
 end
 
-figure(figs)
+figure(airfoil.fig)
