d4a4ef00f998717ae6f227dcdeef3065576a1b92
Lucas Schneeberger <lucas.schneeberger@epfl.ch>
2020-05-14 18:27:41 +0200

    Adapted script to take more ramps according to ramp discoveries. Took in new static curve for flatplate.

--- scripts/ShengCriterion2019.m
+++ scripts/ShengCriterion2019.m
@@ -5,25 +5,44 @@ set(0,'DefaultFigureWindowStyle','docked')
 run('/Users/lucas/src/codes_smarth/labbook.m')
 
 %% Setting up the ramps
-% ms012 cannot be used because inert is not defined
-% ms higher than 17 cannot be used because DS occurs after alpha has
-% reached 30°
-c = [22,27,84,31,34,38];
+
+c = [18,14,22,67,26,84,30,89,34,71,38,93,42,75,46,97,50];
 
 for k=1:length(c)
     data = load(loadmat(LB(c(k)).ms,LB(c(k)).mpt),'raw','inert','avg','zero');
-    avg = data.avg;
-    zero = data.zero;
     raw = data.raw;
-    inert = data.inert;
-    inert.alpha = raw.alpha(raw.t>=0);
-    msname = sprintf('ms%03i',LB(c(k)).ms);
-    assignin('base',msname,RampUpMotion('alpha',inert.alpha,'t',inert.t,'V',LB(c(k)).U));
-    evalin('base',sprintf('%s.setName()',msname))
-    ramp = evalin('base',msname);
+    if LB(c(k)).ms >= 13 && LB(c(k)).ms < 100
+        inert = data.inert;
+        inert.alpha = raw.alpha(raw.t>=0);
+        msname = sprintf('ms%03impt%i',LB(c(k)).ms,LB(c(k)).mpt);
+        assignin('base',msname,RampUpMotion('alpha',inert.alpha,'t',inert.t,'V',LB(c(k)).U));
+        evalin('base',sprintf('%s.setName()',msname))
+        ramp = evalin('base',msname);
+        fc = 35;
+        fs = 1/ramp.Ts;
+        [b,a] = butter(5,fc/(fs/2));
+        Cl_filtered = filter(b,a,inert.Cl);
+        Cd_filtered = filter(b,a,inert.Cd);
+    else
+        msname = sprintf('ms%03impt%i',LB(c(k)).ms,LB(c(k)).mpt);
+        assignin('base',msname,RampUpMotion('alpha',raw.alpha,'t',raw.t,'V',LB(c(k)).U));
+        evalin('base',sprintf('%s.setName()',msname))
+        ramp = evalin('base',msname);
+        fc = 35;
+        fs = 1/ramp.Ts;
+        [b,a] = butter(5,fc/(fs/2));
+        Cl_filtered = filter(b,a,raw.Cl);
+        Cd_filtered = filter(b,a,raw.Cd);
+    end
     ramp.setAlphaDot(LB(c(k)).alphadot) % in degrees
-    ramp.setCL(inert.Cl);
-    ramp.setCD(inert.Cd);
+    Cl_ff = movmean(Cl_filtered,30);
+    Cd_ff = movmean(Cd_filtered,30);
+    fp = 1/3;
+    [b,a] = cheby2(6,20,36*fp/(fs/2));
+    Cl_fff = filter(b,a,Cl_ff);
+    Cd_fff = filter(b,a,Cd_ff);
+    ramp.setCL(Cl_fff);
+    ramp.setCD(Cd_fff);
     ramp.computeAirfoilFrame();
     ramp.isolateRamp();
     % Define stall
@@ -31,12 +50,14 @@ for k=1:length(c)
     evalin('base',sprintf('fig%d = %s.plotCC();',k,msname));
 end
 
+
 %% Running Sheng experiment
 airfoil = Airfoil('flatplate',0.15);
-static = load('static_flatplate');
-airfoil.steady = SteadyCurve(static.alpha,static.Cn,13);
+static = load('data/static_flatplate');
+airfoil.steady = SteadyCurve(static.alpha,static.polarforces.CN,13);
 % Define alpha_ds0 & compute Talpha
-airfoil.Sheng(ms013,ms014,ms034,ms015,ms016,ms017);
+airfoil.Sheng(airfoil,ms012mpt1,ms010mpt1,ms013mpt1,ms025mpt1,ms014mpt1,ms034mpt1,ms015mpt1,ms116mpt1,...
+    ms016mpt1,ms026mpt1,ms017mpt1,ms117mpt1,ms018mpt1,ms027mpt1,ms019mpt1,ms118mpt1,ms020mpt1);
 saveas(gcf,'fig/Sheng/ShengSH2019_dsr.png')
 % %% Add Sheng's predicted stall angles to the figures
 % for k=1:length(c)
