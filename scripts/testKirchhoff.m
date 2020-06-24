4c8ea159d1e9c5cbca50b498d8f73360f17e69c8
Lucas Schneeberger <lucas.schneeberger@epfl.ch>
2020-06-24 10:16:37 +0200

    Adapted script to use relative paths and commented out custom coeffs part.

--- scripts/testKirchhoff.m
+++ scripts/testKirchhoff.m
@@ -1,17 +1,22 @@
 close all
 clear all
 clc
+set(0,'DefaultFigureWindowStyle','docked')
+run('/Users/lucas/src/codes_smarth/labbook.m')
+addpath('../src/model/')
+addpath('../src/common/')
+addpath('../src/lib/')
 
 airfoil = Airfoil('flatplate',0.15);
 
-static = load('static_flatplate.mat');
+static = load('../static_flatplate.mat');
 airfoil.steady = SteadyCurve(static.alpha,static.CN,13.5);
 
-airfoil.steady.fitKirchhoff()
 airfoil.steady.plotKirchhoff()
-
-% Optimal least square: S1=3.3572 S2=2.9679
-plot(airfoil.steady.alpha,kirchhoff(airfoil.steady,airfoil.steady.alpha,[1.5 3]),'DisplayName','custom coeffs','LineWidth',2)
-airfoil.steady.computeSeparation()
 airfoil.steady.plotSeparation()
 
+% % Optimal least square: S1=3.3572 S2=2.9679
+% plot(airfoil.steady.alpha,kirchhoff(airfoil.steady,airfoil.steady.alpha,[0.1 3.2]),'DisplayName','custom coeffs','LineWidth',2)
+% airfoil.steady.computeSeparation()
+% airfoil.steady.plotSeparation()
+% 
