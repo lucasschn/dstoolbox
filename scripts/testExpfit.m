abcc3b3be64e7341e5972a28d9cfd693057e92df
Lucas Schneeberger <lucas.schneeberger@epfl.ch>
2020-06-25 15:23:21 +0200

    Scripts to test the models with optimized parameters

---
+++ scripts/testExpfit.m
@@ -0,0 +1,68 @@
+% This script is test Expfit model on SH2019
+% Author : Lucas Schneeberger
+% Date : 07.06.2020
+
+close all
+clear all
+clc 
+set(0,'DefaultFigureWindowStyle','docked')
+addpath('../plot_dir/')
+addpath('../src/model/')
+addpath('../src/common/')
+addpath('../src/lib/')
+run('/Users/lucas/src/codes_smarth/labbook.m')
+%% Define the airfoil and the associated steady curve
+
+airfoil = Airfoil('flatplate',0.15);
+airfoil.r0 = 0.04;
+static = load('../static_flatplate');
+airfoil.steady = SteadyCurve(static.alpha,static.CN,13.5);
+
+%% Setting up the ramps
+
+c = 22;
+
+for k=1:length(c)
+    if LB(c(k)).ms >= 13 && LB(c(k)).ms < 100
+        data = load(loadmat(LB(c(k)).ms,LB(c(k)).mpt),'raw','inert','avg','zero');
+        raw = data.raw;
+        inert = data.inert;
+        inert.alpha = raw.alpha(raw.t>=0);
+        msname = sprintf('ms%03impt%i',LB(c(k)).ms,LB(c(k)).mpt);
+        assignin('base',msname,RampUpMotion('alpha',inert.alpha,'t',inert.t,'V',LB(c(k)).U,'alphadot',LB(c(k)).alphadot));
+        evalin('base',sprintf('%s.setName()',msname))
+        ramp = evalin('base',msname);
+        Cl = inert.Cl;
+        Cd = inert.Cd;
+    else
+        load(loadmat(LB(c(k)).ms,LB(c(k)).mpt),'raw','zero');
+        msname = sprintf('ms%03impt%i',LB(c(k)).ms,LB(c(k)).mpt);
+        assignin('base',msname,RampUpMotion('alpha',raw.alpha,'t',raw.t,'V',LB(c(k)).U,'alphadot',LB(c(k)).alphadot));
+        evalin('base',sprintf('%s.setName()',msname))
+        ramp = evalin('base',msname);
+        Cl = raw.Cl-zero.Cl;
+        Cd = raw.Cd-zero.Cd;
+    end
+    fs = 1/ramp.Ts;
+    Cl_fff = myFilter(Cl,fs);
+    Cd_fff = myFilter(Cd,fs);
+%     ramp.setCL(Cl);
+%     ramp.setCD(Cd);
+    ramp.setCL(Cl_fff);
+    ramp.setCD(Cd_fff);
+    ramp.computeAirfoilFrame();
+    ramp.isolateRamp();
+    % Define stall
+    ramp.findExpOnset();
+    ramp.setPitchRate(airfoil);
+end
+
+%% Run Expfit-LB model on all ramps
+
+for k=1:length(c) 
+    msname = sprintf('ms%03impt%i',LB(c(k)).ms,LB(c(k)).mpt);
+    evalin('base',sprintf('%s.BLexpfit(airfoil,0.2,1,2,''experimental'')',msname))
+    evalin('base',sprintf('%s.plotLBExpfit(airfoil)',msname))
+    evalin('base',sprintf('%s.plotSeparation(airfoil,''convectime'')',msname))
+end
+
---
+++ scripts/testLB.m
@@ -0,0 +1,55 @@
+% This script is made to test Bangga's model on SH2019
+% Author : Lucas Schneeberger
+% Date : 11.06.2020
+
+close all
+clear all
+clc 
+set(0,'DefaultFigureWindowStyle','docked')
+addpath('../plot_dir/')
+addpath('../src/model/')
+addpath('../src/common/')
+addpath('../src/lib/')
+run('/Users/lucas/src/codes_smarth/labbook.m')
+
+%% Define the airfoil and the associated steady curve
+
+airfoil = Airfoil('flatplate',0.15);
+airfoil.r0 = 0.04;
+static = load('../static_flatplate');
+airfoil.steady = SteadyCurve(static.alpha,static.CN,13.5);
+
+%% Set up the ramp
+
+c = 22;
+
+data = load(loadmat(LB(c).ms,LB(c).mpt),'raw','inert','avg','zero');
+raw = data.raw;
+inert = data.inert;
+inert.alpha = raw.alpha(raw.t>=0);
+msname = sprintf('ms%03impt%i',LB(c).ms,LB(c).mpt);
+ramp = RampUpMotion('alpha',inert.alpha,'t',inert.t,'V',LB(c).U,'alphadot',LB(c).alphadot);
+ramp_filt = RampUpMotion('alpha',inert.alpha,'t',inert.t,'V',LB(c).U,'alphadot',LB(c).alphadot);
+evalin('base',sprintf('ramp.setName(''%s'')',msname))
+evalin('base',sprintf('ramp_filt.setName(''%s'')',msname))
+
+Cl = inert.Cl;
+Cd = inert.Cd;
+fs = 1/ramp.Ts;
+Clf = myFilterTwice(Cl,fs);
+Cdf = myFilterTwice(Cd,fs);
+ramp.setCL(Clf);
+ramp.setCD(Cdf);
+
+ramp.computeAirfoilFrame();
+ramp.isolateRamp();
+% Define stall
+ramp.findExpOnset();
+ramp.setPitchRate(airfoil);
+
+%% Run Bangga's model on the ramp
+
+ramp.BeddoesLeishman(airfoil,8,1,1,0.5,'experimental')
+ramp.plotSteady(airfoil)
+
+% saveas(gcf,'../fig/steady_state_slope13','png')
