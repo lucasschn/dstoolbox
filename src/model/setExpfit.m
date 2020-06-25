6b3bf9c7af0f1528300929410119b3350fdff9af
Lucas Schneeberger <lucas.schneeberger@epfl.ch>
2020-06-25 15:22:13 +0200

    Added computation for fitting error.
    Figure adaptations for report.

--- src/model/setExpfit.m
+++ src/model/setExpfit.m
@@ -37,23 +37,30 @@ xopt = lsqcurvefit(alpha_ds_r,[alpha_ds(end) 1],r,alpha_ds,[0 0],[Inf Inf],opts)
 
 A = xopt(1);
 B = xopt(2);
-save('expfit_flatplate','A','B')
+save('../expfit_flatplate','A','B')
+
+% compute the fitting error
+
+fit_error = sum((alpha_ds - alpha_ds_r([A,B],r)).^2)/length(alpha_ds);
+fprintf('The exponential fitting error is %.2f. \n',fit_error)
 
 % plot alpha_ds(r) and exponential fitting
 
 fig = figure;
 subplot(311)
-plot(r,alpha_ds,'.','DisplayName','\alpha_{ds} (exp)','MarkerSize',20)
+plot(r,alpha_ds,'d','MarkerSize',10,'MarkerFaceColor','k','MarkerEdgeColor','k','DisplayName','\alpha_{ds} (exp)')
 grid on
 ylabel('\alpha_{ds} (°)','FontSize',20);
+axis([0 0.12 0 30]);
 ax = gca;
-axis([0 .06 0 30]);
 ax.FontSize = 20;
 hold on
 plot(r,alpha_ds_r(xopt,r),'LineWidth',2,'DisplayName','exponential fit')
 subplot(313)
 plot(r,t_ds-t_ss,'.','MarkerSize',20)
-xlabel('r (-)')
+xlabel('r')
 ylabel('\Delta t_{ds} (s)')
 grid on
+ax = gca;
+ax.FontSize = 20;
 end
--- src/model/setLinFit.m
+++ src/model/setLinFit.m
@@ -26,7 +26,14 @@ function setLinFit(airfoil,varargin)
                 ramp.findExpOnset();
             end
             % ... and assign it
-            alpha_ds(k) = ramp.alpha_CConset;
+            if ramp.alpha_CConset > airfoil.steady.alpha_ss
+                alpha_ds(k) = ramp.alpha_CConset;
+            elseif ramp.alpha_CLonset > airfoil.steady.alpha_ss
+                alpha_ds(k) = ramp.alpha_CLonset; 
+                warning('CL was used to define stall in %s',ramp.name)
+            else 
+                fit_error('Both CC- and CL-based stall angles are smaller than static stall angle.')
+            end
         end
         
         %% Compute the polynomials for the linear fitting
@@ -54,29 +61,32 @@ function setLinFit(airfoil,varargin)
         % define the critical alpha
         alpha_crit(k) = computeAlphaCrit(airfoil,alpha_ds0,ramp.r);
         end
-
         
+        %% Compute fitting error
+        
+        fit_error = sum((alpha_ds - [polyval(pl,r(r<r0)),polyval(pr,r(r>=r0))]).^2)/length(alpha_ds);
+        fprintf('The linear fitting error is %.2f. \n',fit_error)
         
         %% Plot the alpha_ds_r figure
         
         % plots the results for Sheng's model
         figure
-        % blue dots
-        plot(r,alpha_ds,'.','DisplayName','\alpha_{ds} (exp)','MarkerSize',20)
+        % stall angles (black diamonds)
+        plot(r,alpha_ds,'d','MarkerFaceColor','k','MarkerEdgeColor','k','MarkerSize',12,'DisplayName','\alpha_{ds} (exp)')
         hold on
         grid on
-        xlabel('reduced pitch rate r (-)','FontSize',20);
+        xlabel('r','FontSize',20);
         ylabel('\alpha_{ds} (°)','FontSize',20);
         ax = gca;
-        axis([0 .06 0 30]);
+        axis([0 0.06 12 30]);
         ax.FontSize = 20;
         % red line
         plot(sort([0 r r0]),[alpha_ss polyval(pl,r(r<r0)) polyval(pr,r0) polyval(pr,r(r>=r0))],'Color','r','DisplayName','Linear fitting','LineWidth',2)
         title(sprintf('%s ($T_{\\alpha} = %.2f$)',airfoil.name,Talpha),'interpreter','latex','FontSize',20)
         % yellow dots
-        plot(r,alpha_lag_ds,'.','DisplayName','\alpha_{ds} (lagged)','MarkerSize',20)
+        plot(r,alpha_lag_ds,'d','MarkerSize',12,'MarkerFaceColor','#0072BD','MarkerEdgeColor','#0072BD','DisplayName','\alpha''_{ds}')
         % purple line
-        plot(sort([0 r r0]),sort([alpha_ss alpha_crit alpha_ds0]),'--','DisplayName','\alpha_{crit}','LineWidth',2);
+        plot(sort([0 r r0]),sort([alpha_ss alpha_crit alpha_ds0]),'--','Color',	'#4DBEEE','DisplayName','\alpha_{crit}','LineWidth',2);
         legend('Location','NorthWest','FontSize',20)        
 end
 
