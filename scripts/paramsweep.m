dda23defcb28469e192d79773288621e98e3a89e
Lucas Schneeberger <lucas.schneeberger@gmail.com>
2020-09-01 14:40:30 +0200

    chooses random samples in a range of time constants and computes the model prediction. plots the result distributions in terms of time constants.

---
+++ scripts/paramsweep.m
@@ -0,0 +1,95 @@
+% This script is made to run LB model on a broad range of parameters on
+% SH2019 data
+% Author : Lucas Schneeberger
+% Date : 28.08.2020
+
+close all
+clear all
+clc   
+set(0,'DefaultFigureWindowStyle','docked')
+addpath('../plot_dir/')
+addpath('../src/model/')
+addpath('../src/common/')
+addpath('../src/lib/')
+if ismac  
+    run('/Users/lucas/src/codes_smarth/labbook.m')
+elseif ispc
+    run('labbook')
+end
+%% Define the airfoil and the associated steady curve
+
+airfoil = Airfoil('flatplate',0.15);
+airfoil.r0 = 0.04;
+static = load(fullfile('..','static_flatplate'));
+airfoil.steady = SteadyCurve(static.alpha,static.CN,13);
+
+%% Run the parameter sweep
+params = -ones([1,4]);
+
+c = 71; % list of tested pitch rates
+n = 1000; % number of samples per pitch rate
+
+res = struct('comment',{});
+
+for k = 1:length(c) % loop over the pitch rates
+    data = load(loadmat(LB(c(k)).ms,LB(c(k)).mpt),'raw','inert','avg','zero');
+    raw = data.raw;
+    zero = data.zero;
+    inert.alpha = raw.alpha(raw.t>=0);
+    msname = sprintf('ms%03impt%i',LB(c(k)).ms,LB(c(k)).mpt);
+    ramp = RampUpMotion('alpha',raw.alpha,'t',raw.t,'V',LB(c(k)).U,'alphadot',LB(c(k)).alphadot);
+    evalin('base',sprintf('ramp.setName(''%s'') ',msname))
+
+    Cl = raw.Cl-zero.Cl;
+    Cd = raw.Cd-zero.Cd;
+    fs = 1/ramp.Ts;
+    Clf = myFilterTwice(Cl,fs);
+    Cdf = myFilterTwice(Cd,fs);
+    ramp.setCL(Clf);
+    ramp.setCD(Cdf);
+    
+    ramp.computeAirfoilFrame();
+    ramp.isolateRamp();
+    ramp.setPitchRate(airfoil);
+    % Define stall (convectime must have been set)
+    ramp.findExpOnset();     
+    
+    for kk = 1:n % do n samples per pitch rate
+        params(1) = randsample(200,1)/10;
+        params(2) = randsample(100,1)/10;
+        params(3:4) = randsample(50,2)./10;
+        disp(params)
+        res(n*(k-1)+kk).r = ramp.r;
+        res(n*(k-1)+kk).alphadot = ramp.alphadot;
+        res(n*(k-1)+kk).Tp = params(1);
+        res(n*(k-1)+kk).Tf = params(2);
+        res(n*(k-1)+kk).Tv = params(3);
+        res(n*(k-1)+kk).Tvl = params(4);
+        
+        ramp.BeddoesLeishman(airfoil,params(1),params(2),params(3),params(4),'experimental');
+        
+        % Postprocessing
+        ramp.findPeaks()
+        ramp.computeErr()
+        
+        % Put results in the struct        
+        res(n*(k-1)+kk).CN_LB = ramp.CN_LB;
+        res(n*(k-1)+kk).maxCN = ramp.maxCN;
+        res(n*(k-1)+kk).maxCN_LB = ramp.maxCN_LB;
+        res(n*(k-1)+kk).maxCNk = ramp.maxCNk;
+        res(n*(k-1)+kk).maxCNf = ramp.maxCNf;
+        res(n*(k-1)+kk).maxCNv = ramp.maxCNv;
+        res(n*(k-1)+kk).SmaxCN = ramp.SmaxCN;
+        res(n*(k-1)+kk).SmaxCN_LB = ramp.SmaxCN_LB;
+        res(n*(k-1)+kk).SmaxCNk = ramp.SmaxCNk;
+        res(n*(k-1)+kk).SmaxCNf = ramp.SmaxCNf;
+        res(n*(k-1)+kk).SmaxCNv = ramp.SmaxCNv;
+        res(n*(k-1)+kk).err = ramp.err;
+    end
+end
+
+if ~isfolder(fullfile('..','data','paramsweep'))
+    mkdir(fullfile('..','data','paramsweep'));
+end
+name = 'res25';
+save(fullfile('..','data','paramsweep',name),'res');
---
+++ scripts/plotSweepResults.m
@@ -0,0 +1,90 @@
+% This script is made to plot the results of paramsweep.m
+% Author : Lucas Schneeberger
+% Date : 28.08.2020
+
+close all
+clear all
+clc
+
+load(fullfile('..','data','paramsweep','res'))
+
+plotOneRate(res,25,'Tp','err','Tf')
+
+function plotOneRate(res,rate,varx,vary,color_var)
+res_adot = res(cat(1,res.alphadot)==rate);
+if isempty(res_adot)
+    warning('The specified pitch rate could not be found in the results structure.')
+else
+    for k=1:length(res_adot)
+        x(k) = eval(sprintf('res_adot(k).%s',varx));
+        y(k) = eval(sprintf('res_adot(k).%s',vary));
+    end
+    figure
+    s = scatter(x,y,'filled');
+    grid on
+    xlabel(getLabelString(varx))
+    ylabel(getLabelString(vary))
+    title(sprintf('alphadot = %.2f',rate))
+end
+
+if nargin > 4
+    color = nan(size(res));
+    cvar_min = eval(sprintf('min(cat(1,res_adot.%s))',color_var));
+    cvar_max = eval(sprintf('max(cat(1,res_adot.%s))',color_var));
+    for k = 1:length(res_adot)
+        color(k) = (eval(sprintf('res_adot(k).%s',color_var))-cvar_min)/(cvar_max-cvar_min);
+    end
+    
+    s.CData = eval(sprintf('cat(1,res_adot.%s)',color_var));
+    
+    c = colorbar;
+    c.Ticks = linspace(cvar_min,cvar_max,length(unique(cat(1,sprintf('res.%s',color_var)))));
+    c.Label.String = sprintf('%s',getLabelString(color_var));
+end
+
+end
+
+function plotAllRates(res,x,y)
+color = nan(size(res));
+adot_min = min(cat(1,res.alphadot));
+adot_max = max(cat(1,res.alphadot));
+for k = 1:length(res)
+    color(k) = (res(k).alphadot-adot_min)/(adot_max-adot_min);
+end
+
+figure
+s = scatter(cat(1,res.Tp), cat(1,res.maxCN),'filled');
+grid on
+xlabel('T_p')
+ylabel('max C_N')
+s.CData = color;
+c = colorbar;
+c.Ticks = linspace(0,1,length(unique(cat(1,res.alphadot))));
+c.Label.String = 'pitch rate (°/s)';
+c.TickLabels = unique(color*(adot_max-adot_min)+adot_min);
+end
+
+function label = getLabelString(var)
+switch var
+    case 'Tp'
+        label = 'T_p';
+    case 'Tf'
+        label = 'T_f';
+    case 'Tv'
+        label = 'T_v';
+    case 'Tvl'
+        label = 'T_{vl}';
+    case 'maxCN_LB'
+        label = ' max C_N^{LB}';
+    case 'maxCNf'
+        label = ' max C_N^{f}';
+    case 'maxCNk'
+        label = ' max C_N^{k}';
+    case 'SmaxCN_LB'
+        label = 't_{C,maxLB}';
+    case 'maxCNv'
+        label = 'max C_N^v';
+    otherwise
+        label = var;
+end
+end
