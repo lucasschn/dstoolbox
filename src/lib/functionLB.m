0472db026c1f15b14364b0cc4056ba7e531cac0d
Lucas Schneeberger <lucas.schneeberger@epfl.ch>
2020-09-28 13:52:30 +0200

    Adapted functionLB() to work with pitching motions.

--- src/lib/functionLB.m
+++ src/lib/functionLB.m
@@ -1,4 +1,4 @@
-function out = functionLB(input,LBcoeffs)
+function out = functionLB(input,LBcoeffs,type)
 
 input=checkOptionNP(input);
 LBcoeffs=checkParamNP(LBcoeffs);
@@ -7,29 +7,38 @@ LBcoeffs=checkParamNP(LBcoeffs);
 airfoil = Airfoil('airfoil',input.c);
 airfoil.steady = SteadyCurve(input.static.alpha,input.static.Cn,input.alpha_ss);
 
-% declares a ramp-up experiment
-ramp = RampUpMotion('alpha',input.dyn.alpha,'t',input.dyn.t,'V',input.U,'alphadot',input.alphadot);
-ramp.setCL(input.dyn.Cl)
-ramp.setCD(input.dyn.Cd)
-ramp.computeAirfoilFrame();
+switch type
+    case 'ramp'
+        % declares a ramp-up experiment
+        motion = RampUpMotion('alpha',input.dyn.alpha,'t',input.dyn.t,'V',input.U,'alphadot',input.alphadot);        
+    case 'pitching'
+        motion = PitchingMotion('alpha',input.dyn.alpha,'t',input.dyn.t,'V',input.U,'freq',input.freq);
+        motion.setSinus(airfoil,input.mean_rad,input.amp_rad,input.freq*2*pi);
+    case 'random'
+        motion = AirfoilMotion('alpha',input.dyn.alpha,'t',input.dyn.t,'V',input.U); 
+    otherwise
+end
 
-% uncomment this if you don't want the whole signal but just the ramp part
-%  ramp.isolateRamp();
+motion.setCL(input.dyn.Cl)
+motion.setCD(input.dyn.Cd)
+motion.computeAirfoilFrame();
 
-ramp.BeddoesLeishman(airfoil,LBcoeffs.Tp,LBcoeffs.Tf,LBcoeffs.Tv,LBcoeffs.Tvl,'experimental')
-out = struct(ramp);
+motion.BeddoesLeishman(airfoil,LBcoeffs.Tp,LBcoeffs.Tf,LBcoeffs.Tv,LBcoeffs.Tvl,'experimental')
+out = struct(motion);
 end
 
 function structout = checkOptionNP(structin)
 % check and assign input options
 
 % default values
-default.c = 1; % chord length
+default.c = 1; % chord length in m
 default.static.alpha = linspace(0,20,41);
 default.static.Cn = 2*pi*linspace(0,20,41);
-default.alpha_ss = 12;
-default.U=1;
-default.alphadot=0.2; % in what unit?
+default.alpha_ss = 12; % deg
+default.U=1; % m/s
+default.alphadot=0.2; % deg/s
+default.freq = 1; % Hz
+
 default.dyn.t = linspace(0,20,101);
 default.dyn.alpha = 0.2*linspace(0,20,101); 
 default.dyn.Cl = 2*pi*0.2*linspace(0,20,101);
